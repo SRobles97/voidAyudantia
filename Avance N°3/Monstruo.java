2f1821c1d86d89f48bfaa68f466607ef4cafccd9
SRobles97 <37591168+SRobles97@users.noreply.github.com>
2018-04-18 10:35:59 -0300

    Update Monstruo.java

--- Avance N°3/Monstruo.java
+++ Avance N°3/Monstruo.java
@@ -9,14 +9,27 @@ public class Monstruo {
 	private int def;
 	private int agi;
 	private objetoEquipable drop;
+	private String guild;
 	
 	Monstruo(){
+		this.guild = getNombre(nombresFacciones());		
 		this.hp = aleatorio(3500,4000);
 		this.atk = aleatorio(1000,1500);		
 		this.def = aleatorio(5,25);
 		this.agi = aleatorio(10,100);
 		this.drop = getDrop();
 	}
+	
+    private String[] nombresFacciones() {
+    	String[] nombres = {"Fuego","Agua","Planta"};
+    	return nombres;
+    }	
+	
+    private String getNombre(String[] list) {
+        int a = aleatorio(0,list.length-1);
+        String nombre = list[a];
+        return nombre;
+      }	
 	
 	private int aleatorio(int a, int b) { 
 		Random azar = new Random();
